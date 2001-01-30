f42fc9de917d9eb8ce6923cdd11fb4b56151e06b
Spencer R. Klein <srklein@lbl.gov>
2001-01-30 01:16:06 +0000

    Improved comments/reference to function

--- pams/gen/starlight/src/nrbw.f
+++ pams/gen/starlight/src/nrbw.f
@@ -1,7 +1,7 @@
       DOUBLE PRECISION FUNCTION nrbw(W,ANORM,B,C)
 
 C     Relativistic Breit-Wigner according to J.D. Jackson,
-C     Nuovo Cimento, 1964, with nonresonant term. A is the strength
+C     Nuovo Cimento 34, 6692 (1964), with nonresonant term. A is the strength
 C     of the resnonant term and b the strength of the non-resonant
 C     term. C is an overall normalization. 
 
@@ -14,29 +14,36 @@ C     term. C is an overall normalization.
       DOUBLE PRECISION ppi,ppi0,GammaPrim,rat
       DOUBLE PRECISION aa,bb,cc,dd
 
-c	width depends on energy - Jackson Eq, A.2
+
+
+C  width depends on energy - Jackson Eq. A.2
+
       ppi=DSQRT( (W/2.)**2 - mpi*mpi )
       ppi0=0.358
 
-c	handle phi-K+K- properly
-      if (ip.eq.333) then
-      	ppi=DSQRT( (W/2.)**2 - mK*mK )
-     	ppi0=DSQRT( (mass/2)**2-mK*mK)
+
+C handle phi-->K+K- properly
+      if (ip .eq. 333) then
+         ppi=DSQRT( (W/2.)**2- mK*mK)
+         ppi0=DSQRT( (mass/2)**2-mK*mK)
       endif
 
       rat=ppi/ppi0
       GammaPrim=width*(mass/W)*rat*rat*rat
+
       aa=ANORM*DSQRT(GammaPrim*mass*W)
       bb=W*W-mass*mass
       cc=mass*GammaPrim
       dd=B
-c      nrbw = ( (aa*bb)/(bb*bb+cc*cc) + dd )**2
-c      nrbw = nrbw + ( (aa*cc)/(bb*bb+cc*cc) )**2
-c      nrbw = C*nrbw
-c 	put in a simple, no-background BW, follwoing J. Breitweg et al.
-c	Eq. 15 of Eur. Phys. J. C2, 247 (1998) SRK 11/10/2000
+C      nrbw = ( (aa*bb)/(bb*bb+cc*cc) + dd )**2
+C      nrbw = nrbw + ( (aa*cc)/(bb*bb+cc*cc) )**2
+C  Put in a simple, no-background BW, following J. Breitweg et al.
+C  Eq. 15 of Eur. Phys. J. C2, 247 (1998).  SRK 11/10/2000
+
       nrbw = (ANORM*mass*GammaPrim/(bb*bb+cc*cc))**2
       nrbw = C*nrbw
 
       RETURN
       END
+
+
