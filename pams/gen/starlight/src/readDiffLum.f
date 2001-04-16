ceed1f5014526d3661c42fa757fa7f1cd6a8fa72
Janet E. Seger <jseger@creighton.edu>
2001-04-16 22:01:20 +0000

    normalize F-array

--- pams/gen/starlight/src/readDiffLum.f
+++ pams/gen/starlight/src/readDiffLum.f
@@ -6,11 +6,15 @@ c     parameterized by rapidity and mass
         include 'Ftable.inc'
         include 'inputp.inc'
 
+        REAL f_max
+
 c     numw is the number of w values
 c     numy is the number of y values
 c     warray is array of w values
 c     y is the array of y values
 c     f is the twoD array of f values
+
+      f_max=0.0
 
       open (unit=20,file='starlight.dat',status='unknown')
 
@@ -35,10 +39,18 @@ c     skip first nine entries, Z, A,Gamma,wmax,wmin,numw,ymax,numw,gg_or_gP
       do 300 i = 1,numw
         do 400 j = 1,numy
           read(20,*) farray(i,j)
+          IF( farray(i,j) .gt. f_max )f_max=farray(i,j)
  400    continue
  300  continue
 
       close (unit=20)
+
+C     Normalize farray (JN 010402)
+      do 500 i = 1,numw
+        do 600 j = 1,numy
+          farray(i,j) = farray(i,j)/f_max
+ 600    continue
+ 500  continue
 
       return
       end
