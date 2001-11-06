72b4979eb010e104f333e232a3285a9efd60d94c
Spencer R. Klein <srklein@lbl.gov>
2001-11-06 22:06:08 +0000

    Rewritten in b-dependent approach to include nuclear breakup

--- pams/gen/starlight/src/flux.f
+++ pams/gen/starlight/src/flux.f
@@ -15,6 +15,8 @@ C  rewritten 4/25/2001 by SRK
 
       include 'D2LParam.inc'
       include 'const.inc'
+      include 'inputp.inc'
+
       DOUBLE PRECISION Egamma,lEgamma,Emin,Emax,lnEmin,lnEmax
       DOUBLE PRECISION stepmult,energy,rZ
       INTEGER j,nbstep,jb,nrstep,jr,nphistep,jphi,nstep
@@ -25,16 +27,117 @@ C  rewritten 4/25/2001 by SRK
 
       DOUBLE PRECISION Xvar,Dbesk1
 
+      DOUBLE PRECISION binc,ra,phad,P1N,PXN,omax
+
       INTEGER Icheck,I,Ilt
+
+      INTEGER ILOW,IHIGH
+      DOUBLE PRECISION deltab,brange,singlep,bin
+
+      DOUBLE PRECISION gammatarg,rhad,pnohad
+
+C  This holds the 'chosen' table
+      DOUBLE PRECISION prob(1070),b1(1070)
 
       SAVE Icheck,dide,lnEMax,lnEmin,dlnE
 
       DATA Icheck/0/
 
+C   first call?  - initialize - calculate photon flux
+
       Icheck=Icheck+1
       IF(Icheck.gt.1)GOTO 1000
 
-C   first call - calculate photon flux
+      rz=float(Z)
+      ra=float(A)
+
+C  is nuclear breakup considered?
+
+      if (ibreakup .eq. 1) then
+         write(6,111)
+ 111     format(' no nuclear breakup criteria')
+         goto 220
+      endif
+
+
+C  !!!!Breakup is considered!!!!!
+
+
+C  determine hadronic breakup probability from hadronbreakup.f
+C  determine EM breakup probability from photonbreakup.f
+
+C  start lookup table at bmin=2R_A; multiplicative steps of 1.1%
+      bmin=2.*Rnuc
+      binc=10.**.005
+
+C  different photon energy cutoffs for RHIC & LHC
+
+      omax=1.E7
+      if (gamma_em .gt. 500) omax=1.E9
+
+
+C  find gamma in target system
+
+      gammatarg=2.*gamma_em*gamma_em-1.
+
+C  both nuclei break up (XnXn)
+
+           if (ibreakup .eq. 2) THEN
+              write(6,213)bmin
+ 213      FORMAT(' Requiring both nuclei to break up (XnXn). ',
+     *'Coulomb Only. bmin= ',F7.3)
+
+              do 203 j=1,996
+                 bmin=bmin*binc
+                 call hadronbreakup(rhad,bmin,rz,ra,gamma_em)
+                 call photonbreakup(P1N,PXN,bmin,rz,ra,gammatarg,omax)
+                 b1(j)=bmin
+                 pnohad=exp(-rhad)
+ 203             prob(j)= (1.-exp(-PXN))*(1-exp(-PXN))*pnohad
+              goto 220
+
+           ENDIF
+
+C ibreakup=3  --> both nuclear emit 1 neutron  (1n1n)
+
+           if (ibreakup .eq. 3) THEN
+
+              WRITE(6,214)bmin
+ 214   FORMAT(' Req. both nuclei to emit 1 neutron (1n1n) . bmin = '
+     *,F7.3)
+
+              do 204 j=1,996
+                 bmin=bmin*binc
+                 call hadronbreakup(rhad,bmin,rz,ra,gamma_em)
+                 call photonbreakup(P1N,PXN,bmin,rz,ra,gammatarg,omax)
+                 b1(j)=bmin
+                 pnohad=exp(-rhad)                 
+
+C  old version
+C 204             prob(j)= (1.-exp(-P1N))*(1-exp(-P1N))*pnohad
+
+C following Eq. 7 of Baltz, Klein & Nystrand
+C the 1n excitation cannot be accompanied by Xn
+ 204             prob(j)=(exp(P1N-PXN)-exp(-PXN))**2*pnohad
+
+              goto 220
+           ENDIF
+
+C ibreakup=4 --> neither nucleus is excited
+
+           if (ibreakup .eq. 4) THEN
+              do 205 j=1,996
+                 bmin=bmin*binc
+                 call hadronbreakup(rhad,bmin,rz,ra,gamma_em)
+                 call photonbreakup(P1N,PXN,bmin,rz,ra,gammatarg,omax)
+                 b1(j)=bmin
+                 pnohad=exp(-rhad)
+ 205             prob(j)= pnohad*exp(-PXN)*exp(-PXN)
+              WRITE(6,215)
+ 215          FORMAT(' Requiring that neither nucleus break up.')
+           ENDIF
+
+ 220       continue
 
 C  collect number of integration steps here, in one place
 
@@ -47,10 +150,10 @@ C  following previous choices, take Emin=10 keV at LHC, Emin = 1 MeV at RHIC
         Emin=1.E-5
         if (gamma_em .lt. 500) Emin=1.E-3
 
-C  maximum energy is 10 times the cutoff
+C  maximum energy is 12 times the cutoff
 C  25 GeV for gold at RHIC, 650 GeV for lead at LHC
 
-        Emax=10.*hbarc*gamma_em/Rnuc
+        Emax=12.*hbarc*gamma_em/Rnuc
 
 C     >> lnEmin <-> ln(Egamma) for the 0th bin
 C     >> lnEmax <-> ln(Egamma) for the last bin
@@ -59,11 +162,9 @@ C     >> lnEmax <-> ln(Egamma) for the last bin
         lnEmax=DLOG(Emax)
         dlnE=(lnEmax-lnEmin)/100.
 
-        rZ=float(Z)
-
         write(6,5)Emin,Emax
  5      format(' flux.f.  Calculating flux for photon energies', 
-     &'from E=',E11.3,' to ',E11.3,'.')
+     &'from E=',E11.3,' to ',E11.3,' GeV (lab frame).')
 
 C  100 steps in energy
 
@@ -154,6 +255,20 @@ C  85    format(' b<2R_A = ',F7.3,' fluxelement= ',E11.3,' Xvar= ',E11.3)
 C  multiply by volume element to get total flux in the volume element
 
              fluxelement=fluxelement*2.*pi*biter*(biter-bold)
+
+C  modulate by the probability of nuclear breakup as f(biter)
+
+             IF (ibreakup .gt. 1) THEN
+                 bin = 0.5 + DLOG(biter/10.)/(0.005*LOG(10.))
+                 ILOW = INT(bin)
+                 IHIGH = ILOW + 1
+                 brange = b1(IHIGH)-b1(ILOW)
+                 deltab = biter - b1(ILOW)        
+                 singlep = (1-(deltab/brange))*prob(ILOW) + 
+     &(deltab/brange)*prob(IHIGH)
+                 fluxelement=fluxelement*singlep
+              ENDIF
+
              integratedflux=integratedflux+fluxelement
 
  90          continue
