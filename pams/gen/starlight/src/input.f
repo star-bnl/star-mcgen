579de8933dc2557184fa950ff268e0ac76201286
Spencer R. Klein <srklein@lbl.gov>
2002-12-09 23:29:20 +0000

    Version 2.0 - additional input parameters.

--- pams/gen/starlight/src/input.f
+++ pams/gen/starlight/src/input.f
@@ -7,15 +7,16 @@ c     See starlight.doc for an explanation of input parameters
 
       include 'inputp.inc'
       include 'D2LParam.inc'
+      include 'pttable.inc'
 
       open (unit=15,file='starlight.in',status='unknown')
       read (15,*) Z
       read (15,*) A
       read (15,*) gamma_em
-      read (15,*) wmax
+      read (15,*) Wmax
       read (15,*) Wmin
       read (15,*) numw
-      read (15,*) ymax
+      read (15,*) Ymax
       read (15,*) numy
       read (15,*) gg_or_gP
       read (15,*) ievents
@@ -27,6 +28,14 @@ C added 4/12/2001 by SRK ibreakup: 1=don't care; 2= mutual excitation
 C 3= no excitation 4=exactly 1 excitation 5=1+ excitation
 
       read (15,*) ibreakup
+
+C  added 8/2002 by SRK for interference
+
+      read(15,*) iinterfere
+      read(15,*) xinterfere
+      read(15,*) ptmax
+      read(15,*) NPT
+      dpt = ptmax/DFLOAT(NPT)
       close (unit=15)
 
       return
