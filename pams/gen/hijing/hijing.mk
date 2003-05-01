cefe7023ed99445980b162b40a524738da7c67b8
Ronald Longacre <longacre>
2003-05-01 15:56:47 +0000

    add rzmake.f to make file

--- pams/gen/hijing/hijing.mk
+++ pams/gen/hijing/hijing.mk
@@ -1,5 +1,5 @@
 
-SOURCES = hijjet.f hipyset.f hijev.f heptup.f hijing.f makhvy.f
+SOURCES = hijjet.f hipyset.f hijev.f heptup.f hijing.f makhvy.f rzmake.f
 
 # Include files (for printout, below)
 INCFILES = 
