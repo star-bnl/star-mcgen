25d2a8f2b462f196f351316656ccd99b58a11430
klendathu2k <jwebb@bnl.gov>
2021-09-28 14:34:14 -0400

    Revert "Merge pull request #1 from klendathu2k/pythia8.2.35-xmlpath-patch"
    This reverts commit 5178d649ceb1e6a48368447420110427a48fe86e, reversing
    changes made to 5cfd402bf3280879cb2fcb223609e033906d040c.

--- StRoot/StarGenerator/Pythia8_2_35/StarPythia8.cxx
+++ StRoot/StarGenerator/Pythia8_2_35/StarPythia8.cxx
@@ -30,7 +30,7 @@ StarPythia8::StarPythia8(const char *name) : StarGenerator(name)
   // auxilliary files required by pythia. 
   // NOTE:  When adding new versions of Pythia8, we need to specify
   // the version of the code in the source code
-  TString path = "StRoot/StarGenerator/"; path+= Pythia8_version; path+="/Share/xmldoc/"; 
+  TString path = "StRoot/StarGenerator/"; path+= Pythia8_version; path+="/xmldoc/"; 
   { 
     ifstream in(path); 
     if (!in.good()) { path = "$(STAR)/"+path; }
